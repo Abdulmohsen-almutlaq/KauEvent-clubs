3929b77dce934d428e4ce8b6b5a8842f86420c07
abdulmohsen almutlaq <almutlaq002@gmail.com>
2023-11-25 23:45:29 +0300

    test

--- testing/src/testing/Admin.java
+++ testing/src/testing/Admin.java
@@ -1,16 +1,16 @@
 package testing;
 
 import java.sql.*;
+import java.util.Date;
 
 public class Admin {
 
     private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=KAU_Events_Clubs;user=sa;password=12345";
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
     private static String admin_KAU_ID;
 
-    /**
-     *
-     */
     public Admin() {
         // Constructor if needed
     }
@@ -18,124 +18,107 @@ public class Admin {
     public static boolean validateAdmin(String admin_KAU_ID, String adminPassword) {
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection con = DriverManager.getConnection(DB_URL)) {
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
                 String sql = "SELECT * FROM AdminBase WHERE admin_KAU_ID=? AND adminPassword=?";
                 try (PreparedStatement pst = con.prepareStatement(sql)) {
                     pst.setString(1, admin_KAU_ID);
                     pst.setString(2, adminPassword);
                     try (ResultSet rs = pst.executeQuery()) {
                         if (rs.next()) {
-                            // If a matching record is found, set the details in the current instance
-                            Admin.admin_KAU_ID = rs.getString("admin_KAU_ID");
-                            // Set other fields as needed
+                            admin_KAU_ID = rs.getString("admin_KAU_ID");
                             return true;
                         }
                     }
                 }
             }
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace(); // Handle or log the exception as needed
+            e.printStackTrace();
         }
         return false;
     }
 
-public boolean insertEvent(String eventName, String eventDescription, java.util.Date eventDate, String Admin_ID) {
-    try {
-        // Load the JDBC driver
-        Class.forName(JDBC_DRIVER);
+    public boolean insertEvent(String eventName, String eventDescription, Date eventDate, String Admin_ID) {
+        try {
+            Class.forName(JDBC_DRIVER);
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                String query = "INSERT INTO eventBase (eventName, eventDescription, eventDate, Admin_ID) VALUES (?, ?, ?, ?)";
 
-        // Establish a connection to the database
-        try (Connection con = DriverManager.getConnection(DB_URL)) {
-            // Define the SQL query for event insertion
-            String query = "insert into eventBase (eventName, eventDescription, eventDate, Admin_ID) values (?, ?, ?, ?)";
+                try (PreparedStatement pst = con.prepareStatement(query)) {
+                    pst.setString(1, eventName);
+                    pst.setString(2, eventDescription);
+                    java.sql.Date sqlEventDate = new java.sql.Date(eventDate.getTime());
+                    pst.setDate(3, sqlEventDate);
+                    pst.setString(4, Admin_ID);
 
-            // Create a PreparedStatement to safely execute the query
-            try (PreparedStatement pst = con.prepareStatement(query)) {
-                // Set values for the parameters in the query
-                pst.setString(1, eventName);
-                pst.setString(2, eventDescription);
-                java.sql.Date sqlEventDate = new java.sql.Date(eventDate.getTime());
-                pst.setDate(3, sqlEventDate);
-                pst.setString(4, Admin_ID);
+                    pst.executeUpdate();
 
-                // Execute the update (insert) operation
-                pst.executeUpdate();
-
-                // Return true indicating successful event insertion
-                return true;
+                    return true;
+                }
             }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-    } catch (ClassNotFoundException | SQLException e) {
-        // Log or handle the exception as needed
-        e.printStackTrace();
-    }
-
-    // Return false indicating event insertion failure
-    return false;
-}
-
-
-public static boolean deleteEventFromDatabase(String eventName) {
-    try {
-        Class.forName(JDBC_DRIVER);
-        try (Connection con = DriverManager.getConnection(DB_URL)) {
-            String query = "DELETE FROM eventBase WHERE eventName = ?";
-
-            try (PreparedStatement pst = con.prepareStatement(query)) {
-                pst.setString(1, eventName);
-                int rowsAffected = pst.executeUpdate();
-
-                // Check if the deletion was successful
-                return rowsAffected > 0;
-            }
-        }
-    } catch (ClassNotFoundException | SQLException e) {
-        e.printStackTrace(); // Log the exception or handle it as needed
         return false;
     }
-}
 
-public static boolean updateEventInDatabase(String eventName, String newEventName, String eventDescription, java.util.Date eventDate, String adminID) {
-    try {
-        Class.forName(JDBC_DRIVER);
-        try (Connection con = DriverManager.getConnection(DB_URL)) {
-            String query = "UPDATE eventBase SET eventName=?, eventDescription=?, eventDate=?, Admin_ID=? WHERE eventName=?";
+    public static boolean deleteEventFromDatabase(String eventName) {
+        try {
+            Class.forName(JDBC_DRIVER);
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                String query = "DELETE FROM eventBase WHERE eventName = ?";
 
-            try (PreparedStatement pst = con.prepareStatement(query)) {
-                pst.setString(1, newEventName);
-                pst.setString(2, eventDescription);
-                java.sql.Date sqlEventDate = new java.sql.Date(eventDate.getTime());
-                pst.setDate(3, sqlEventDate);
-                pst.setString(4, adminID);
-                pst.setString(5, eventName);
+                try (PreparedStatement pst = con.prepareStatement(query)) {
+                    pst.setString(1, eventName);
 
-                int rowsAffected = pst.executeUpdate();
+                    int rowsAffected = pst.executeUpdate();
 
-                // Check if the update was successful
-                return rowsAffected > 0;
+                    return rowsAffected > 0;
+                }
             }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
         }
-    } catch (ClassNotFoundException | SQLException e) {
-        e.printStackTrace(); // Log the exception or handle it as needed
-        return false;
     }
-}
+
+    public static boolean updateEventInDatabase(String eventName, String newEventName, String eventDescription, Date eventDate, String adminID) {
+        try {
+            Class.forName(JDBC_DRIVER);
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                String query = "UPDATE eventBase SET eventName=?, eventDescription=?, eventDate=?, Admin_ID=? WHERE eventName=?";
+
+                try (PreparedStatement pst = con.prepareStatement(query)) {
+                    pst.setString(1, newEventName);
+                    pst.setString(2, eventDescription);
+                    java.sql.Date sqlEventDate = new java.sql.Date(eventDate.getTime());
+                    pst.setDate(3, sqlEventDate);
+                    pst.setString(4, adminID);
+                    pst.setString(5, eventName);
+
+                    int rowsAffected = pst.executeUpdate();
+
+                    return rowsAffected > 0;
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     public boolean checkIfEventExistsInTable(String eventName) throws SQLException {
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection con = DriverManager.getConnection(DB_URL)) {
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
                 String query = "SELECT * FROM eventBase WHERE eventName=?";
                 try (PreparedStatement pst = con.prepareStatement(query)) {
                     pst.setString(1, eventName);
                     try (ResultSet rs = pst.executeQuery()) {
-                        // Check if a record with the given event name exists
                         return rs.next();
                     }
                 }
             }
         } catch (ClassNotFoundException | SQLException e) {
-            // Log or handle the exception as needed
             throw new SQLException("Error checking if event exists in table", e);
         }
     }
@@ -143,8 +126,8 @@ public static boolean updateEventInDatabase(String eventName, String newEventNam
     public static boolean insertClub(String clubCode, String clubName, String clubDescription, Date clubFounded) {
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection con = DriverManager.getConnection(DB_URL)) {
-                String query = "insert into clubBase (clubCode,clubName,clubDescription,clubFounded)values(?,?,?,?)";
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                String query = "INSERT INTO clubBase (clubCode, clubName, clubDescription, clubFounded) VALUES (?, ?, ?, ?)";
 
                 try (PreparedStatement pst = con.prepareStatement(query)) {
                     int clubCodeInt = Integer.parseInt(clubCode);
@@ -154,22 +137,22 @@ public static boolean updateEventInDatabase(String eventName, String newEventNam
 
                     java.sql.Date sqlClubFounded = new java.sql.Date(clubFounded.getTime());
                     pst.setDate(4, sqlClubFounded);
+
                     pst.executeUpdate();
 
-                    return true; // Club insertion successful
+                    return true;
                 }
             }
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace(); // Log or handle the exception as needed
+            e.printStackTrace();
         }
-
-        return false; // Club insertion failed
+        return false;
     }
 
-    public static  boolean updateClub(String clubCode, String newClubCode, String clubName, String clubDescription, Date clubFounded) {
+    public static boolean updateClub(String clubCode, String newClubCode, String clubName, String clubDescription, Date clubFounded) {
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection con = DriverManager.getConnection(DB_URL)) {
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
                 String query = "UPDATE clubBase SET clubCode=?, clubName=?, clubDescription=?, clubFounded=? WHERE clubCode=?";
 
                 try (PreparedStatement pst = con.prepareStatement(query)) {
@@ -183,12 +166,11 @@ public static boolean updateEventInDatabase(String eventName, String newEventNam
 
                     int rowsAffected = pst.executeUpdate();
 
-                    // Check if the update was successful
                     return rowsAffected > 0;
                 }
             }
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e.getMessage());// Log the exception or handle it as needed
+            e.printStackTrace();
             return false;
         }
     }
@@ -196,28 +178,27 @@ public static boolean updateEventInDatabase(String eventName, String newEventNam
     public static boolean deleteClub(String clubCode) {
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection con = DriverManager.getConnection(DB_URL)) {
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
                 String query = "DELETE FROM clubBase WHERE clubCode=?";
                 try (PreparedStatement pst = con.prepareStatement(query)) {
                     pst.setString(1, clubCode);
+
                     int rowsAffected = pst.executeUpdate();
 
-                    // Check if the deletion was successful
                     return rowsAffected > 0;
                 }
             }
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             return false;
         }
     }
 
     public static AdminFunctions loginAdmin(String admin_KAU_ID, String adminPassword) {
         if (validateAdmin(admin_KAU_ID, adminPassword)) {
-            // If validation is successful, create an instance of AdminFunctions
             return new AdminFunctions();
         } else {
-            return null; // Return null if validation fails
+            return null;
         }
     }
 }
